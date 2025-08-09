509948fee8ef2e9fb755bb4927e2382ba79345e6
2420030763-sriram <2420030763@klh.edu.in>
2025-08-09 09:27:40 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,17 +1,31 @@
-package Lab_4;
+import java.util.*;
 
-class Student {
- private int rollNumber;    
- protected String grade;    
- public String name;        
- public void setDetails(int rollNumber, String grade, String name) {
-     this.rollNumber = rollNumber;
-     this.grade = grade;
-     this.name = name;
- }
- public void showDetails() {
-     System.out.println("Roll Number: " + rollNumber);
-     System.out.println("Grade: " + grade);
-     System.out.println("Name: " + name);
- }
+public class Student {
+    private long rollNumber;    
+    public String name;        
+
+    public void setDetails(long rollNumber, String grade, String name) {
+        this.rollNumber = rollNumber;
+        this.grade = grade;
+        this.name = name;
+    }
+
+    public void showDetails() {
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Grade: " + grade);
+        System.out.println("Name: " + name);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        // Create an object of Student
+        Student s1 = new Student();
+        
+        // Set student details
+        s1.setDetails(2420030763L, "A", "SRIRAMA KRISHNAJI");
+        
+        // Show student details
+        s1.showDetails();
+    }
 }
